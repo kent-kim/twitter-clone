e1abcf857718ffcc27d3b1040f003521a9ea7558
kent-kim <76205965+kent-kim@users.noreply.github.com>
2022-04-21 19:20:48 -0500

    update

---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Sidebar from "../components/Sidebar"
+import Feed from '../components/Feed'
+import {getProviders, getSession, useSession} from "next-auth/react"
+import Login from '../components/Login'
+import Modal from '../components/Modal'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
+import Widgets from '../components/Widgets'
+
+interface HomeProps  {
+  trendingResults:any,
+  followResults:any,
+  providers:any
+}
+
+const Home: NextPage<HomeProps> = (props) => {
+  const {trendingResults,followResults,providers} = props
+  const {data:session} = useSession();
+  const [isOpen,setIsOpen] = useRecoilState(modalState)
+
+  if(!session) return <Login providers={providers}/>
+
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
+        <Sidebar/>
+        <Feed />
+            <Widgets trendingResults={trendingResults} followResults={followResults}/>
+        {isOpen && <Modal/>}
+      </main>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context:any){
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then((res)=>res.json())
+  
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then((res)=>res.json())
+
+  const providers = await getProviders();
+  console.log(providers)
+  const session = await getSession(context)
+
+  return{
+    props:{
+      trendingResults,
+      followResults,
+      providers,
+      session
+    }
+  }
+}
+
+
+
+export default Home
